6f78b0768e97a00c98effcd564ab2d0643cd8cb4
Dr Internet <jonjon1234.github@gmail.com>
2015-03-08 21:22:15 +0000

    ErrorHold Commented
    Comments added to WalkingHoldError.py, to make it easier for you to
    follow.

--- WalkingHoldError.py
+++ WalkingHoldError.py
@@ -1,28 +1,29 @@
-import os
-import datetime
-from time import sleep as slp
-scanfile = 0
-scanned = []
+import os # Import OS, for os.walk
+import datetime # Import datetime, for datetime.now function.
+from time import sleep as slp # Import the sleep function
+scanfile = 0 # We haven't scanned any files yet.
+scanned = [] # And we havn't scanned any drives.
+start = datetime.datetime.now() # Record the start time.
 
-try:
-    start = datetime.datetime.now()
-    for i in range(65, 91):
-        path = chr(i)+"://"
-        if os.path.exists(path):
-            scanned.append("Drive Scanned: "+chr(i))
-            for dir_, _, files in os.walk(path):
-                for file in files:
-                    print(os.path.join(dir_, file))
-                    slp(0.005)
-                    scanfile = scanfile + 1
-    end = datetime.datetime.now()
-    running = end - start
-    print()
-    print("Files Scanned: "+str(scanfile))
-    print("Time to Complete: "+str(running))
-    for a in scanned:
-        print(a)
-    input("Press Enter to finish")
-except BaseError as errored:
-    print(errored)
-    input()
+try: # Yay for error catching.
+    start = datetime.datetime.now() # Record the start time, again?
+    for i in range(65, 91): # Find the drives.
+        path = chr(i)+"://" # Make them real paths.
+        if os.path.exists(path): # Check for media.
+            scanned.append("Drive Scanned: "+chr(i)) # Add it to the scanned drive list.
+            for dir_, _, files in os.walk(path): # Then walk through the drive.
+                for file in files: # For every file.
+                    print(os.path.join(dir_, file)) # Print to the screen.
+                    slp(0.005) # Then sleep for a split second.
+                    scanfile = scanfile + 1 # And we scanned another file.
+    end = datetime.datetime.now() # Record the end time.
+    running = end - start # And therefore the time taken.
+    print() # Make a nice new line.
+    print("Files Scanned: "+str(scanfile)) # We scanned XYZ files.
+    print("Time to Complete: "+str(running)) # And it took ABC minutes.
+    for a in scanned: # Finally, make a nice drives scanned listing.
+        print(a) # As above.
+    input("Press Enter to finish") # Then make an input to finish.
+except BaseError as errored: # BUT, if we error.
+    print(errored) # Catch and print the error for me.
+    input() # Then call an input to keep the console up.
